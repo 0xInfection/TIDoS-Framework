17559dbef5a0ae924837599d48cc31d76e8deab3
theinfecteddrake <theinfecteddrake@gmail.com>
2018-06-20 12:49:05 +0100

    Updated for the better range

--- core/serbugs.py
+++ core/serbugs.py
@@ -19,6 +19,7 @@ from rce import *
 from csrf import *
 from vulnban1 import *
 from sqli import *
+from crlf import *
 from hhi import *
 from vulnban import *
 from shellshock import *
@@ -76,18 +77,24 @@ def serbugs(web):
 	serbugs(web)
 
     elif v == '8':
+	print ' Type Selected : HTTP Response Splitting'
+	crlf(web)
+	time.sleep(1)	
+	serbugs(web)
+
+    elif v == '9':
 	print ' Type Selected : Host Header Injection'
 	hhi(web)	
 	time.sleep(1)	
 	serbugs(web)
 
-    elif v == '9':
+    elif v == '10':
 	print ' Type Selected : Shellshock'
 	shellshock(web)
 	time.sleep(1)	
 	serbugs(web)
 
- #   elif v == '10':
+ #   elif v == '11':
 #	print ' Type Selected : URL Validation'
 #	redirect(web)
 #	time.sleep(1)	
@@ -130,6 +137,11 @@ def serbugs(web):
 	print ' [*] Firing up module --> SQLi'
 	sqli(web)
 	print ' [!] Module Completed --> SQLi\n'
+	time.sleep(1)
+
+	print ' [*] Firing up module --> CRLF'
+	crlf(web)
+	print ' [!] Module Completed --> CRLF\n'
 	time.sleep(1)
 
 	print ' [*] Firing up module --> Host Header Injection'
