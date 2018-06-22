fb71b6b9e946a0f16b0a26e94c8dae54cc57222e
theinfecteddrake <theinfecteddrake@gmail.com>
2018-06-22 13:50:59 +0100

    Bug fixes (common ones)

--- core/inputin.py
+++ core/inputin.py
@@ -15,31 +15,38 @@ from colors import *
 
 def inputin():
 
-	web = raw_input(''+O+' [#] Target web address :> '+C+'')
-	global web
-	if 'http' not in str(web):
-		mo = raw_input(GR+' [#] Does this website use SSL? (y/n) :> ')
-		if mo == 'y' or mo == 'Y':
-			web = 'https://'+web
-		elif mo == 'n':
-			web = 'http://'+web
-	if 'http://' in web:
-		po = web.replace('http://','')
-	elif 'https://' in web:
-		po = web.replace('https://','')
-	if web.endswith('/'):
-		web = web[:-1]
-	print GR+' [*] Checking server status...'
-	time.sleep(0.6)
 	try:
-		ip = socket.gethostbyname(po)
-		print G+' [+] Site seems to be up...'
-		time.sleep(0.5)
-		print G+' [+] IP Detected : '+O+ip
-		time.sleep(0.5)
-		print ''
-		return web
-	except socket.gaierror:
-		print R+' [-] Site seems to be down...'
-		sys.exit(1)
+		web = raw_input(''+O+' [#] Target web address :> '+C+'')
+		global web
+		if 'http' not in str(web):
+			mo = raw_input(GR+' [#] Does this website use SSL? (y/n) :> ')
+			if mo == 'y' or mo == 'Y':
+				web = 'https://'+web
+			elif mo == 'n':
+				web = 'http://'+web
+		if 'http://' in web:
+			po = web.replace('http://','')
+		elif 'https://' in web:
+			po = web.replace('https://','')
+		if web.endswith('/'):
+			web = web[:-1]
+		print GR+' [*] Checking server status...'
+		time.sleep(0.6)
+		try:
+			ip = socket.gethostbyname(po)
+			print G+' [+] Site seems to be up...'
+			time.sleep(0.5)
+			print G+' [+] IP Detected : '+O+ip
+			time.sleep(0.5)
+			print ''
+			return web
+		except socket.gaierror:
+			print R+' [-] Site seems to be down...'
+			sys.exit(1)
 
+	except KeyboardInterrupt:
+		print R+' [-] Exiting...'
+		time.sleep(0.7)
+		print C+' [#] Alvida, my friend!'
+		sys.exit(1)
+	
