f65ed49421c7694c43d199b909162b4e46a92b55
theinfecteddrake <theinfecteddrake@gmail.com>
2018-06-23 10:58:02 +0100

    Update brutemods_ for more functions

--- core/brutemods.py
+++ core/brutemods.py
@@ -23,6 +23,9 @@ from auxil_alt import *
 from sshbrute import *
 from brutemodsban import *
 from colors import *
+from smtpbrute import *
+from xmppbrute import *
+from telnetbrute import *
 
 def brutemods(web):
 
@@ -35,28 +38,49 @@ def brutemods(web):
     if v == '1':
 	print B+' [!] Type Selected :'+C+' FTP Brute'
 	ftpbrute(web)
-	time.sleep(1)
+	time.sleep(2)
 	print '\n\n'
 	auxil_alt(web)
 
     elif v == '2':
 	print B+' [!] Type Selected :'+C+' SSH Brute'
 	sshbrute(web)
-	time.sleep(1)
+	time.sleep(2)
 	print '\n\n'
 	auxil_alt(web)
 
     elif v == '3':
 	print B+' [!] Type Selected :'+C+' SQL Brute'
 	sqlbrute(web)
-	time.sleep(1)
+	time.sleep(2)
 	print '\n\n'
 	auxil_alt(web)
 
     elif v == '4':
 	print B+' [!] Type Selected :'+C+' POP 3/2 Brute'
 	popbrute(web)
-	time.sleep(1)
+	time.sleep(2)
+	print '\n\n'
+	auxil_alt(web)
+
+    elif v == '5':
+	print B+' [!] Type Selected :'+C+' SMTP Brute'
+	smtpbrute(web)
+	time.sleep(2)
+	print '\n\n'
+	auxil_alt(web)
+
+    elif v == '6':
+	print B+' [!] Type Selected :'+C+' TELNET Brute'
+	telnetbrute(web)
+	time.sleep(2)
+	print '\n\n'
+	auxil_alt(web)
+
+    elif v == '7':
+	print B+' [!] Type Selected :'+C+' XMPP Brute'
+	xmppbrute(web)
+	time.sleep(2)
 	print '\n\n'
 	auxil_alt(web)
 
@@ -70,20 +94,35 @@ def brutemods(web):
 	print B+' [*] Firing up module -->'+C+' SSH Brute'
 	sshbrute(web)
 	print B+' [!] Module Completed -->'+C+' SSH Brute\n'
-	time.sleep(1)
+	time.sleep(2)
 
 	print B+' [*] Firing up module -->'+C+' SQL Brute'
 	sqlbrute(web)
 	print B+' [!] Module Completed -->'+C+' SQL Brute\n'
-	time.sleep(1)
+	time.sleep(2)
 
 	print B+' [*] Firing up module -->'+C+' POP Brute'
 	popbrute(web)
 	print B+' [!] Module Completed -->'+C+' POP Brute\n'
-	time.sleep(1)
+	time.sleep(2)
+
+	print B+' [*] Firing up module -->'+C+' SMTP Brute'
+	smtpbrute(web)
+	print B+' [!] Module Completed -->'+C+' SMTP Brute\n'
+	time.sleep(2)
+
+	print B+' [*] Firing up module -->'+C+' TElNET Brute'
+	telnetbrute(web)
+	print B+' [!] Module Completed -->'+C+' TELNET Brute\n'
+	time.sleep(2)
+
+	print B+' [*] Firing up module -->'+C+' XMPP Brute'
+	xmppbrute(web)
+	print B+' [!] Module Completed -->'+C+' XMPP Brute\n'
+	time.sleep(2)
 
 	print B+' [!] All scantypes have been tested on target...'
-	time.sleep(1)
+	time.sleep(4)
 	print B+' [*] Going back to menu...'
 	auxil_alt(web)
 
