3ff4824c037563fa75e799e0928a606038dd37db
theinfecteddrake <theinfecteddrake@gmail.com>
2018-06-26 10:58:39 +0100

    Added illuance

--- runon.sh
+++ runon.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+#    TIDoS Framework     #
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+
+#This module requires TIDoS Framework
+#https://github.com/theInfectedDrake/TIDoS
+
 cd /opt/tidos-framework
 python /opt/tidos-framework/tidos.py
