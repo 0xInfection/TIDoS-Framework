bd2aff99431572c9cb2c5023fa45f2167414579d
theinfecteddrake <theinfecteddrake@gmail.com>
2018-06-27 14:02:07 +0100

    Updated the remainder code functions

--- core/tidos_main.py
+++ core/tidos_main.py
@@ -82,7 +82,7 @@ def tidos_main():
 			print C+' [+] Alvida, see ya!\n'
 			sys.exit(0)
 
-		#except Exception as e:
+		except Exception as e:
 			print R+' [-] Something happened! :('
 			print R+' [!] Error : '+str(e)
 
--- modules/VulnLysis/MiscBugs/cloudflare.py
+++ modules/VulnLysis/MiscBugs/cloudflare.py
@@ -19,17 +19,14 @@ from re import search
 from colors import *
 br = mechanize.Browser()
 
-# Cookie Jar
 cj = cookielib.LWPCookieJar()
 br.set_cookiejar(cj)
 
-# Browser options
 br.set_handle_equiv(True)
 br.set_handle_redirect(True)
 br.set_handle_referer(True)
 br.set_handle_robots(False)
 
-# Follows refresh 0 but not hangs on refresh > 0
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [
     ('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
