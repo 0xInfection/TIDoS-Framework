bd8040f253485c5f647df98d565cab9e371f1140
theinfecteddrake <theinfecteddrake@gmail.com>
2018-07-02 06:17:32 +0100

    Improvements to the email_ext_module

--- modules/InfoDisc/emailext.py
+++ modules/InfoDisc/emailext.py
@@ -23,16 +23,19 @@ def mail0x00(url):
     print R+'    ======================\n'
     time.sleep(0.5)
     print GR+' [*] Making the request...'
-    req = requests.get(url, verify=False)
+    req = requests.get(url, allow_redirects=False, timeout=10)
     print O+' [*] Harvesting emails...'
     time.sleep(1)
-    search = re.findall(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", req.content)
-    for i in search:
-        print G+" [+] E-mail found : "+O, str(i)
+    text = req.text
+    patron = re.compile("[-a-zA-Z0-9._]+@[-a-zA-Z0-9_]+.[a-zA-Z0-9_.]+")
+    mails = re.findall(patron,text)
+    for mail in mails:
+	print G+' [+] Found : '+O+mail
 
 def emailext(web):
 
 	print GR+' [*] Loading module...'
 	time.sleep(0.6)
 	mail0x00(web)
+	print G+'\n [+] Done!\n'
 
