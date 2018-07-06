f94515777abc943f8a4dcc02f1a4b819184ff226
theinfecteddrake <theinfecteddrake@gmail.com>
2018-07-06 13:17:18 +0100

    Improvements to the CMS Module

--- modules/ActiveRecon/cms.py
+++ modules/ActiveRecon/cms.py
@@ -33,40 +33,33 @@ br.set_handle_referer(True)
 br.set_handle_robots(False)
 
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.addheaders = [
-    ('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+whatcms_token = '756ab2cfa1ed5575a71e0714ef05c2e228f17b6b1476de7075f7f4d6b4978272376fb3'
 
 def getcmslook(web):
 
 	global found
 	global dtect
 	global wordpress
+	web = web.replace('http://','')
+	web = web.replace('https://','')
 	print GR+' [*] Looking up for the CMS...'
 	time.sleep(1)
-        result = br.open('https://whatcms.org/?s=' + domain).read()
-        found = search(r'">[^<]*</a><a href="/New-Detection', result)
         wordpress = False
 	dtect = False
+	request = requests.get('https://whatcms.org/APIEndpoint/Detect?url=' + web + '&key=' + whatcms_token, verify=False)
+	response = json.loads(request.text)
 
-        try:
-            r = br.open(web + '/robots.txt').read()
-            if "wp-admin" in str(r):
-                wordpress = True
-		dtect = True
-        except:
-            pass
+	status = response['result']['code']
 
-	try:
-	    r = requests.get(web+'/wp-login.php')
-	    if str(r.status_code) == '200':
-	        wordpress = True
-		dtect = True
-	    else:
-		wordpress = False
-
-	except Exception as e:
-		print R+' [-] Exception handling failed!'
-		print R+' [-] Error : '+str(e)
+	if 'retry_in_seconds' in response:
+		print R+' [-] Outbound Query Exception!'
+	else:
+		if status == 200:
+			dtect = True
+			print G+'[-] CMS detected: ' +O+ response['result']['name']
+		else:
+			dtect = False
 
 def cmsenum(web):
 
@@ -78,7 +71,7 @@ def cmsenum(web):
 	r = json.loads(res)
 	try:
 	    if "cms" in r:
-		print G+' [+] CMS Detected : %s' % (r['cms'])
+		print G+' [+] CMS Detected :'+O+' %s' % (r['cms'])
 		dtect = True
 		time.sleep(0.7)
 
@@ -107,15 +100,6 @@ def cms(web):
 	time.sleep(0.2)
 	getcmslook(web)
 	cmsenum(web)
-
-        if found:
-            print G+" [+] CMS Detected : " + found.group().split('">')[1][:-27]
-            found = found.group().split('">')[1][:-27]
-
-        elif wordpress == True:
-	    print O+' [!] Website seems to use Wordpress...'
-	    time.sleep(0.2)
-            print G+" [+] CMS Detected : WordPress"
 
         if dtect == False:
             print R+" [-] "+O + domain+R + " doesn't seem to use a CMS"
