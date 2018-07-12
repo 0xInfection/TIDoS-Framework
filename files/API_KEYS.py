55330b6abb3997136c3b259aabe3890d68e95cb6
theinfecteddrake <theinfecteddrake@gmail.com>
2018-07-12 05:37:43 +0100

    Added new payloads for db

---
+++ files/API_KEYS.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*- 
+
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+#    TIDoS Framework     #
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+
+#This module requires TIDoS Framework
+#https://github.com/theInfectedDrake/TIDoS-Framework
+
+SHODAN_API_KEY = 'v4YpsPUJ3wjDxEqywwu6aF5OZKWj8kik' #Public Shodan API Key Bundled with TIDoS-Framework
+CENSYS_UID = ''
+CENSYS_SECRET = ''
+FULLCONTACT_API_KEY = ''
+WHATCMS_API_KEY = ''
