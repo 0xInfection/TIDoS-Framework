bb8050ce62e240c5045075fa9611a450960af0ff
@_tID <39941993+theInfectedDrake@users.noreply.github.com>
2018-07-20 01:18:39 +0530

    Fixed module numbers respectively...

--- core/dispmenu.py
+++ core/dispmenu.py
@@ -15,11 +15,11 @@ def dispmenu():
 	
 	print ''+O+'\n Choose from the options below :\n'
 	time.sleep(0.2)
-	print ''+B+' [1] \033[1;36mReconnaissance & OSINT'+W+' (36 modules)'
+	print ''+B+' [1] \033[1;36mReconnaissance & OSINT'+W+' (43 modules)'
 	time.sleep(0.1)
-	print ''+B+' [2] \033[1;36mScanning & Enumeration'+W+' (28 modules)'
+	print ''+B+' [2] \033[1;36mScanning & Enumeration'+W+' (13 modules)'
 	time.sleep(0.1)
-	print ''+B+' [3] \033[1;36mVulnerability Analysis'+W+' (36 modules)'
+	print ''+B+' [3] \033[1;36mVulnerability Analysis'+W+' (35 modules)'
 	time.sleep(0.1)
 	print ''+B+' [4] \033[1;36mExploitation (beta)'+W+' (only 1)'
 	time.sleep(0.1)
