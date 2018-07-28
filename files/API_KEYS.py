977dd121653dc80022659833db9c5d801fe1a820
theinfecteddrake <theinfecteddrake@gmail.com>
2018-07-28 09:55:55 +0100

    Edited the api keys file

--- files/API_KEYS.py
+++ files/API_KEYS.py
@@ -11,7 +11,7 @@
 SHODAN_API_KEY = 'v4YpsPUJ3wjDxEqywwu6aF5OZKWj8kik' #Public Shodan API Key Bundled with TIDoS-Framework
 CENSYS_UID = ''
 CENSYS_SECRET = ''
-GOOGLE_ACCESS_TOKEN = ''
+GOOGLE_API_TOKEN = ''
 FULLCONTACT_API_KEY = ''
 WHATCMS_ACCESS_TOKEN = '756ab2cfa1ed5575a71e0714ef05c2e228f17b6b1476de7075f7f4d6b4978272376fb3' 
-					#Public WHATCMS API Key Bundled with TIDoS-Framework
+					#Public WHATCMS Access Token Bundled with TIDoS-Framework
