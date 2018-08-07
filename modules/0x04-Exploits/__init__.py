e7052bf6a6be0761398793dfcf4184fc37206e52
theinfecteddrake <theinfecteddrake@gmail.com>
2018-08-07 09:49:19 +0100

    Added build for Exploitation Phase phase

---
+++ modules/0x04-Exploits/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# coding:  utf-8
+
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+#    TIDoS Framework     #
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+
+#This module requires TIDoS Framework
+#https://github.com/theInfectedDrake/TIDoS-Framework 
+
+pass
