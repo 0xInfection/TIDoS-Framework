b643757a3ce09f938e8a10ec68c37bbd11072ff1
theinfecteddrake <theinfecteddrake@gmail.com>
2018-08-30 10:28:34 +0100

    Added code for new module sslscan

--- core/Enumeration/scanenumban.py
+++ core/Enumeration/scanenumban.py
@@ -40,10 +40,11 @@ def scanenumban():
 \033[1;34m [2] \033[1;36mPort Scanning and Analysis \033[0m(Several Types)
 \033[1;34m [3] \033[1;36mInteractive Scanning with NMap \033[0m(16 Preloaded modules)
 \033[1;34m [4] \033[1;36mWeb Technologies Enumeration\033[0m(FrontEnd Technologies)
-\033[1;34m [5] \033[1;36mOperating System Enumeration \033[0m(Absolute)
-\033[1;34m [6] \033[1;36mGrab Banners on Services \033[0m(via Open Ports)
-\033[1;34m [7] \033[1;36mScan all IP Addresses Linked to Domain \033[0m(CENSYS)
-\033[1;34m [8] \033[1;36mLet loose Crawlers on the target \033[0m(Depth 1, 2 & 3)
+\033[1;34m [5] \033[1;36mRemote Server SSL Enumeration\033[0m(Absolute)
+\033[1;34m [6] \033[1;36mOperating System Enumeration \033[0m(Absolute)
+\033[1;34m [7] \033[1;36mGrab Banners on Services \033[0m(via Open Ports)
+\033[1;34m [8] \033[1;36mScan all IP Addresses Linked to Domain \033[0m(CENSYS)
+\033[1;34m [9] \033[1;36mLet loose Crawlers on the target \033[0m(Depth 1, 2 & 3)
  
 \033[1;34m [A] \033[1;36mAutomate all one by one on target
 
