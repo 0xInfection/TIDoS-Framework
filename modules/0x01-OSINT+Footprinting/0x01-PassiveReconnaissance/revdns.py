07e3dcf02edd0622dd15c221c3c6c3a0b35edb6a
0xInfection <theinfecteddrake@gmail.com>
2018-11-30 11:21:08 +0000

    Fixes 0x01 of #59

--- modules/0x01-OSINT+Footprinting/0x01-PassiveReconnaissance/revdns.py
+++ modules/0x01-OSINT+Footprinting/0x01-PassiveReconnaissance/revdns.py
@@ -18,8 +18,7 @@ links = []
 
 def revdns(web):
 
-    web = web.replace('http://','')
-    web = web.replace('https://','')
+    web = web.split('//')[1]
     print(R+'\n   =====================================')
     print(R+'    R E V E R S E   D N S   L O O K U P')
     print(R+'   =====================================\n')
@@ -27,32 +26,29 @@ def revdns(web):
     print('' + GR + color.BOLD + ' [!] Looking Up for Reverse DNS Info...')
     time.sleep(0.4)
     print(""+ GR + color.BOLD + " [~] Result: \n"+ color.END)
-    domains = [web]
-    for dom in domains:
-        text = requests.get('http://api.hackertarget.com/reversedns/?q=' + dom).text
-        result = str(text)
-        if 'error' not in result:
-            res = result.splitlines()
-            for r in res:
-                print(B+' [+] Received : '+O+r.split(' ')[0].strip()+' => '+C+'('+r.split(' ')[1].strip()+')')
-                time.sleep(0.04)
-                links.append(r)
+    text = requests.get('http://api.hackertarget.com/reversedns/?q=' + web)
+    result = text.text
+    if 'error' not in result and 'no results' not in result.lower():
+        res = result.splitlines()
+        for r in res:
+            print(B+' [+] Received : '+O+r.split(',')[0].strip()+' => '+C+'('+r.split(',')[1].strip()+')')
+            time.sleep(0.04)
+            links.append(r)
 
-            p = 'tmp/logs/'+web+'-logs/'+web+'-reverse-dns.lst'
-            open(p,'w+')
-            print(B+' [!] Saving links...')
-            time.sleep(1)
-            for m in links:
-                m = m + '\n'
-                ile = open(p,"a")
-                ile.write(m)
-                ile.close()
-            pa = os.getcwd()
-            print(G+' [+] Links saved under '+pa+'/'+p+'!')
-            print('')
+        p = 'tmp/logs/'+web+'-logs/'+web+'-reverse-dns.lst'
+        open(p,'w+')
+        print(B+' [!] Saving links...')
+        time.sleep(1)
+        for m in links:
+            m = m + '\n'
+            ile = open(p,"a")
+            ile.write(m)
+            ile.close()
+        pa = os.getcwd()
+        print(G+' [+] Links saved under '+pa+'/'+p+'!')
+        print('')
 
-        elif 'No results found' in result:
-            print(R+' [-] No result found!')
-        else:
-            print(R+' [-] Outbound Query Exception!')
-            time.sleep(0.8)
+    else:
+        print(R+' [-] No result found!')
+        time.sleep(0.8)
+revdns('http://criteo.com')
