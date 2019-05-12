9fb3533cce9cffb16777a500cf0b9f1425155f17
cleong14 <root@localhost.localdomain>
2019-05-12 07:12:35 -0400

    Enum4linux module successfully runs command. TODO: 1. Add multiprocess functionality 2. Add command options 3. Add colors

--- threat/modules/enumeration/windows/enum4linux.py
+++ threat/modules/enumeration/windows/enum4linux.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+import subprocess
+
 def enum4linux(target):
     target[0].ip = '10.10.10.125'       # temp value
     target[0].port = '445'              # temp value
+
+    enum4linux_str = 'enum4linux ' + target[0].ip
+
+    print(enum4linux_str)
+
+    process = subprocess.run(['enum4linux', '10.10.10.125'], check=True, stdout=subprocess.PIPE, universal_newlines=True)
+    output = process.stdout
+
+    print('PROCESS', process)
+    print('OUTPUT', output)
