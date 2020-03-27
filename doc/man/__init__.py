322bab2c2d941cd4533df15e23aba51d2c8658a0
Raoul Scholtes <s8rlscho@stud.uni-saarland.de>
2020-03-27 22:04:04 +0100

    cleanup

--- doc/man/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from core import *
-
-pass
--- lib/scapy_mod/bin/UTscapy.bat
+++
@@ -1,2 +0,0 @@
-@REM Use Python to run the UTscapy script from the current directory, passing all parameters
-@python %~dp0\UTscapy %*
