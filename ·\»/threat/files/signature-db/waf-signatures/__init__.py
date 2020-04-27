2ae8174408ad4d65050034511721f3f1f3dcb248
VainlyStrain <vainlystrain@gmail.com>
2020-04-27 21:10:29 +0200

    deleted dev folder

--- ·\»/threat/files/signature-db/waf-signatures/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-#
-# @name:    Wascan - Web Application Scanner
-# @repo:    https://github.com/m4ll0k/Wascan
-# @author:  Momo Outaadi (M4ll0k)
-# @license: See the file 'LICENSE.txt
